fe2db24d34ff1d8bd95c970466ae3a0797b75d25
JinhyeonK <diamondataJH@gmail.com>
2025-05-09 10:06:07 +0900

    Title: 합과 평균, Time: 136ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250509/합과 평균/sum-and-avg.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    int a,b,t;
+    Scanner sc = new Scanner(System.in);
+    a= sc.nextInt();
+    b= sc.nextInt();
+    t=a+b;
+    double c=(a+b)/2.0;
+    System.out.println(t+" "+c);
+
+    }
+}

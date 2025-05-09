52068844f623c85d214cd010496b4c55632a349a
JinhyeonK <diamondataJH@gmail.com>
2025-05-09 12:35:18 +0900

    Title: 점수 비교, Time: 128ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250509/점수 비교/score-comparison.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+    Scanner sc = new Scanner(System.in);
+    int am,ae,bm,be;
+    am=sc.nextInt();
+    ae=sc.nextInt();
+    bm=sc.nextInt();
+    be=sc.nextInt();
+    if (am>bm&&ae>be){
+        System.out.println(1);
+    }else{
+         System.out.println(0);
+
+    }
+    
+    
+    }
+}

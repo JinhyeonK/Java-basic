b77f69af7dfd5189726171bb69ebbfa3fdd8d7ae
JinhyeonK <diamondataJH@gmail.com>
2025-05-11 17:42:08 +0900

    Title: 100점 도달하기, Time: 135ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250511/100점 도달하기/reach-100-point.java
+++ 250511/100점 도달하기/reach-100-point.java
@@ -2,19 +2,24 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Please write your code here.
-        Scanner sc = new scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
         for(int i=0;n<=100;i++){
-            if (n>=90){
-                System.out.println("A");
-            } elif (n>=80){
-                System.out.println("B");
-            } elif (n>=70){
-                System.out.println("C");
-            } elif (n=60){
-                System.out.println("D");
-            } else{
-                System.out.println("F");
+            if (n>=90) {
+                System.out.print("A ");
+                n+=i;
+            } else if (n>=80) {
+                System.out.print("B ");
+                n+=i;
+            } else if (n>=70) {
+                System.out.print("C ");
+                n+=i;
+            } else if (n>=60) {
+                System.out.print("D ");
+                n+=i;
+            } else {
+                System.out.print("F ");
+                n+=i;
         }
     }
 }
